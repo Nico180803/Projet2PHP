bb92484bf69727383b12edf02832a907fb965b81
Nicolas CHARPENTIER <CHARPENTIER_Ni@lprs.local>
2024-11-29 08:26:46 +0100

    feature : page d'accueil et liste des livres

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
 <?php
+session_start();
+?>
+<head>
+    <meta charset="UTF-8">
+    <title>BIBLIOTHEQUE-ACCUEIL</title>
+</head>
+<body>
+<hr>
+<a href="">S'inscrire</a>
+<a href="">Se connecter</a>
+<a href="">Liste des Livres</a>
+<hr>
+<h1>BIENVENUE</h1>
+</body>
